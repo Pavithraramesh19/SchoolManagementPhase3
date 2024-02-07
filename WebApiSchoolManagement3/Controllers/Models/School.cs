7a5b6269ecf14cc9fb271fb13d932a5bdd2acb02
Pavithraramesh19 <pavithraramesh1902@gmail.com>
2024-02-07 18:53:04 +0530

    changes

---
+++ WebApiSchoolManagement3/Controllers/Models/School.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiSchoolManagement3.Controllers.Models
+{
+    [Table("School")]
+    public class School
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Subject { get; set; }
+        public int Marks { get; set; }
+    }
+}
---
+++ WebApiSchoolManagement3/Data/SchoolDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiSchoolManagement3.Controllers.Models;
+
+namespace WebApiSchoolManagement3.Data
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext (DbContextOptions<SchoolDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiSchoolManagement3.Controllers.Models.School> School { get; set; } = default!;
+    }
+}
